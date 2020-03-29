8ac79050499f17f7fb1d146e22f1ca5977b7f6ed
iqbalelyas <iqbal35mm@gmail.com>
2020-03-29 16:00:03 +0700

    deploy

---
+++ precache-manifest.ff7eefd129922c7d2e2d079fcb353345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b83cce4fe5f8c69d27ac0139fd7973d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad59b0e61d286aa2af86",
+    "url": "/static/css/main.28e79960.chunk.css"
+  },
+  {
+    "revision": "b911dbf03c3c5d0d44c7",
+    "url": "/static/js/2.fb99d3f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb99d3f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad59b0e61d286aa2af86",
+    "url": "/static/js/main.16e2ab32.chunk.js"
+  },
+  {
+    "revision": "372986bb63ea5d476bd1",
+    "url": "/static/js/runtime-main.a3ebe3be.js"
+  }
+]);
